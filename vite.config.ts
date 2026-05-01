df19a86b343d1ee22429b5e03ba873c6419fa737
Cristian Ismael Prado Salazar <ismadev.computer@gmail.com>
2026-05-01 15:05:23 -0600

    ruta repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/guadalupe-armenta-odontologia/', 
   plugins: [
     react(),
     tailwindcss(),
